a3b5a3d93b1cd2090a513fca87492434ae480bb8
Rodrigo Marchioro <rodrigomarchioro@hotmail.com>
2025-02-12 21:14:42 -0300

    Initial commit

---
+++ src/main/java/com/rodrigo/magaluAgendamentoNotificacao_API/MagaluAgendamentoNotificacaoApiApplication.java
@@ -0,0 +1,13 @@
+package com.rodrigo.magaluAgendamentoNotificacao_API;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MagaluAgendamentoNotificacaoApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MagaluAgendamentoNotificacaoApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/rodrigo/magaluAgendamentoNotificacao_API/MagaluAgendamentoNotificacaoApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rodrigo.magaluAgendamentoNotificacao_API;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MagaluAgendamentoNotificacaoApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
